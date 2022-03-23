e0c77e628f4ba1f1022af40e01ba0a11ab04dbd4
James Swank <jmswank7@gmail.com>
2022-03-23 06:33:20 -0600

    update

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ from datetime import date
 
 
 # df = pd.read_csv(r'C:\Users\jswank\Desktop\File2.csv')
-df = pd.read_csv('File2.csv')
+df = pd.read_csv('file2.csv')
 # print(df)
 df = df.astype('Int64')
 df = df.astype('str')
 df = df.sort_values(by='CreationDate_1')
 
-df = df.iloc[-5000:]
+# #df = df.iloc[-5000:]
 # print(df)
 
 df = df.applymap(lambda x: pd.to_datetime(str(x),format='%Y%m%d', errors='coerce'))
@@ -32,7 +32,7 @@ df = df.applymap(lambda x: pd.to_datetime(str(x),format='%Y%m%d', errors='coerce
 # df.report_minus_collection = df.report_minus_collection / np.timedelta64(1, 'D')
 
 # df = df[df['report_minus_collection'] >= 0]
-# print(df)
+print(df)
 
 
 def get_layout():
@@ -45,7 +45,7 @@ def get_layout():
             ),
             html.Div([
                 html.Div([
-                    dcc.Graph(id='histogram1')
+                    dcc.Graph(id='creation-minus-report-hist')
                 ],
                     className='eight columns'
                 ),
@@ -59,7 +59,32 @@ def get_layout():
                 ),
             ],
                 className='row'
-            )
+            ),
+            html.Div([
+                html.Div([
+                    dcc.Graph(id='report-minus-collection-hist')
+                ],
+                    className='eight columns'
+                ),
+                html.Div(id='counts')
+                # html.Div([
+                #     html.H6('Count of Records in Selected Date Range')
+                # ],
+                #     className='four columns'
+                # ),
+            ],
+                className='row'
+            ),
+            # html.Div([
+            #     html.Div([], className='one column'),
+            #     html.Div([
+            #         html.H6('Analysis of the 339 new death cases downloaded from CEDRS with a death date of 3-21-2022')
+            #     ],
+            #         className='eight columns'
+            #     ),
+            # ],
+            #     className='row'
+            # ),
         ]
     )
 
@@ -67,8 +92,12 @@ app = dash.Dash(__name__)
 app.layout = get_layout
 app.config['suppress_callback_exceptions']=True
 
+
+
 @app.callback(
-    Output('histogram1', 'figure'),
+    Output('counts', 'children'),
+    Output('creation-minus-report-hist', 'figure'),
+    Output('report-minus-collection-hist', 'figure'),
     Input('date', 'end_date'),
     Input('date', 'start_date'))
 def update_histogram1(end_date, start_date):
@@ -84,33 +113,49 @@ def update_histogram1(end_date, start_date):
     df_test.report_minus_collection = df_test.report_minus_collection / np.timedelta64(1, 'D')
     print(df_test)
     df_test = df_test[df_test['report_minus_collection'] >= 0]
-    
-    traces = []
+    count = len(df_test.index)
+    print(count)
 
-    traces.append(go.Histogram(
+    
+    cmrh_traces = []
+    rmch_traces = []
+
+    cmrh_traces.append(go.Histogram(
         x=df_test['create_minus_report']
     ))
 
-    layout = go.Layout(
+    cmrh_layout = go.Layout(
         paper_bgcolor="#1f2630",
         plot_bgcolor="#1f2630",
         font=dict(color="#2cfec1"),
-        bargap=0.2
+        bargap=0.2,
+        xaxis={'title':'Difference in Creation and Report Dates in Days'}
+    )
+
+    rmch_traces.append(go.Histogram(
+        x=df_test['report_minus_collection']
+    ))
+
+    rmch_layout = go.Layout(
+        paper_bgcolor="#1f2630",
+        plot_bgcolor="#1f2630",
+        font=dict(color="#2cfec1"),
+        bargap=0.2,
+        xaxis={'title':'Difference in Report and Collection Dates in Days'}
     )
 
 
-    return {'data': traces, 'layout': layout}
-    # data = 
+    return html.Div([
+        html.Div([
+            html.H6('Count of Records in Selected Date Range'),
+            html.H6('{}'.format(count))
+        ],
+            className='four columns'
+        ),
+    ],
+        className='row'
+    ), {'data': cmrh_traces, 'layout': cmrh_layout}, {'data': rmch_traces, 'layout': rmch_layout}
 
-
-# fig = px.histogram(df, x=df['create_minus_report'],
-#                     title='Difference In Days Between Creation Date <br> and Report Date for Cases Reported Between 3-4-2022 and 3-11-2022',
-#                     labels={'create_minus_report': 'Days', 'Count':'Count'},
-#                     text_auto=True)
-
-# fig.update_layout(bargap=0.2)
-
-# fig.show()
 
 if __name__ == "__main__":  
     app.run_server(port=8080, debug=True)
